62768c87c75879c1b2e8ac74373ebbd27a4bc586
Adrienne Franke <adriennefranke@adriennes-mbp.attlocal.net>
2017-02-27 18:33:31 -0600

    kitty commit

--- pitchers95.py
+++ pitchers95.py
@@ -5,26 +5,30 @@ import requests
 import json
 # from bs4 import BeautifulSoup
 # players/a/alstowa01.shtml?redir
+# //*[@id="misc_pitching_clone"]/tbody/tr[1]/td
 
-
-page = requests.get('http://www.baseball-reference.com/leagues/MLB/1995-rookies.shtml#pitch')
-tree = html.fromstring(page.content)
-cleanLink = list()
-response = []
-
-
-links = tree.xpath('//*[@id="misc_pitching"]/tbody/tr/td/a/@href')
-print(links)
-for link in links:
-    cleanLink.append(link)
-
-print(cleanLink)
-
-for count in range(len(cleanLink)):
-	page = requests.get('http://www.baseball-reference.com/'+cleanLink[count])
+def getThosePitchers(the_year):
+	page = requests.get('http://www.baseball-reference.com/leagues/MLB/'+str(the_year)+'-rookies.shtml#pitch')
 	tree = html.fromstring(page.content)
-	playerName = str(tree.xpath('//*[@id="player_name"]/text()')); so = str(tree.xpath('//*[@id="pitching_standard.1995"]/td[22]/text()')); ip = str(tree.xpath('//*[@id="pitching_standard.1995"]/td[15]/text()')); print(playerName, so, ip); response.append({'Player' : playerName, 'SO' : so, "IP" : ip})
+	cleanLink = list()
+	response = []
 
-with open("pitcherStats.json", 'w') as outfile:
-	json.dump(response, outfile, indent=4)
-outfile.close()
+
+	links = tree.xpath('//*[@id="misc_pitching"]/tbody/tr/td/a/@href')
+	print(links)
+	for link in links:
+	    cleanLink.append(link)
+
+	print(cleanLink)
+
+	for count in range(len(cleanLink)):
+		page = requests.get('http://www.baseball-reference.com/'+cleanLink[count])
+		tree = html.fromstring(page.content)
+		playerName = str(tree.xpath('//*[@id="misc_pitching_clone"]/tbody/tr[1]/td/text()')); so = str(tree.xpath('//*[@id="pitching_standard.1995"]/td[22]/text()')); ip = str(tree.xpath('//*[@id="pitching_standard.1995"]/td[15]/text()')); print(playerName, so, ip); response.append({'Player' : playerName, 'SO' : so, "IP" : ip})
+
+	with open(str(the_year)+"pitcherStats.json", 'w') as outfile:
+		json.dump(response, outfile, indent=4)
+	outfile.close()
+
+for i in range(22):
+	getThosePitchers(1994+i)
