3830744cda71f8385a86c8ee8c774a08fd82949a
John Ottenlips <johno@aloompa.com>
2017-03-28 11:10:10 -0500

    working on cleaning and getting leaders

--- leadersnhl.py
+++ leadersnhl.py
@@ -7,34 +7,37 @@ import json
 # from bs4 import BeautifulSoup
 # players/a/alstowa01.shtml?redir
 
-
+#
 # //*[@id="my-players-table"]/div[1]/div[1]/div[2]/table/tbody
-
+#
 # //*[@id="my-players-table"]/div[1]/div[2]/div/table/tbody
 # //*[@id="my-players-table"]/div[1]
 # //*[@id="my-players-table"]/div[1]/div[3]/div/table/tbody
 
+# //*[@id="my-players-table"]/div[1]/div[1]/div[2]/table
 
-# page = requests.get('http://www.espn.com/nhl/statistics')
+
+page = requests.get('http://www.espn.com/nhl/statistics')
+tree = html.fromstring(page.content)
+cleanLinks = list()
+response = []
+
+links = tree.xpath('//*[@id="my-players-table"]/div[1]/div[1]/div[2]/table//*/td/a/@href')
+cleanLinks = links[2:-1]
+print(cleanLinks)
+
+for link in cleanLinks:
+
+
+# page = requests.get('http://www.espn.com/nhl/stats/dailyleaders')
 # tree = html.fromstring(page.content)
 # cleanLink = list()
 # response = []
 #
-# linkTitles = tree.xpath('//*/div[@class="mod-content"]//*/a/text()')
-# links = tree.xpath('//*/div[@class="mod-content"]//*/a/@href')
-# print(links)
-
-
-
-page = requests.get('http://www.espn.com/nhl/stats/dailyleaders')
-tree = html.fromstring(page.content)
-cleanLink = list()
-response = []
-
-linkTitles = tree.xpath('//*[@id="my-players-table"]/div[1]/div[1]/table[2]//*/td[2]/a/text()')
-links = tree.xpath('//*[@id="my-players-table"]/div[1]/div[1]/table[2]//*/td[2]/a/@href')
-print(linkTitles, len(links))
-
+# linkTitles = tree.xpath('//*[@id="my-players-table"]/div[1]/div[1]/table[2]//*/td[2]/a/text()')
+# links = tree.xpath('//*[@id="my-players-table"]/div[1]/div[1]/table[2]//*/td[2]/a/@href')
+# print(linkTitles, len(links))
+#
 
 # with open("baseball-reference.json", 'wb') as outfile:
 # 	json.dump(response, outfile, indent=4)
